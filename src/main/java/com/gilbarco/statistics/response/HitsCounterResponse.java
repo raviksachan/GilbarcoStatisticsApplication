016bd6d08e6f854aeb8a127c9298fc7c27ae5a5e
Ravi Kant <ravi@zepo.in>
2018-10-12 00:50:27 +0530

    Hits Count Rest Api: Code Formatting correction

--- src/main/java/com/gilbarco/statistics/response/HitsCounterResponse.java
+++ src/main/java/com/gilbarco/statistics/response/HitsCounterResponse.java
@@ -2,26 +2,26 @@ package com.gilbarco.statistics.response;
 
 public class HitsCounterResponse {
 	/**
-     * The current ISO8601 timestamp.
-     */
+	 * The current ISO8601 timestamp.
+	 */
 	private String timestamp;
-	
+
 	/**
-     * The total number of hit calls
-     */
+	 * The total number of hit calls
+	 */
 	private int calls;
 
 	/**
-     * Constructs a new HitsCounterResponse with the specified timestamp and total number of hit calls.
-     *
-     * @param  timeStamp 	current ISO8601 timestamp
-     * @param  calls   		total number of hit calls
-     */
+	 * Constructs a new HitsCounterResponse with the specified timestamp and total number of hit calls.
+	 *
+	 * @param timeStamp		current ISO8601 timestamp
+	 * @param calls			total number of hit calls
+	 */
 	public HitsCounterResponse(String timeStamp, int calls) {
 		this.timestamp = timeStamp;
 		this.calls = calls;
 	}
-	
+
 	public String getTimestamp() {
 		return timestamp;
 	}
